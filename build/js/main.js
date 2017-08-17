72e94f695093528284dde9ac1ce17cfad2f73c9f
Maxim <Maxim>
2017-08-17 17:06:05 +0300

    first commit

---
+++ build/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+    // Если в проекте используются встроенные js-плагины от Foundation, разкомментировать
+    // $(document).foundation();
+
+
+    $('.menu-open').click(function () {
+        $('.menu-list').toggle(300);
+    })
+
+});
